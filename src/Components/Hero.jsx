91ec8b75c5aca0d65ac174e5a84841b150c4eb33
Fpitkat <75321987+Fpitkat@users.noreply.github.com>
2022-05-22 18:08:47 -0400

    Typewriter

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,10 +1,42 @@
+import TypewriterComponent from "typewriter-effect";
+
 export default function Hero() {
   return (
     <section className="hero--section">
       <div className="hero--text">
         <p className="hero--title">
-          Welcome to Sandy Paws{" "}
-          <span className="text--secondary after">Animal Hospital</span>
+          Welcome to Sandy Paws
+          <span className="hero--type">
+            <TypewriterComponent
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Animal Hospital")
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString("Wellness Exams")
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString("Vaccinations")
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString("Dentistry")
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString("Diagnostics")
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString("Surgery")
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString("Cold laser therapy")
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString("Animal Hospital")
+                  .pauseFor(1000)
+                  .start();
+              }}
+            />
+          </span>
         </p>
         <p className="hero--content">
           Sandy Paws Animal Hospital is a full-service veterinary hospital that
