58fe4d44933ba34df55bb690217d7b190171a03b
Fpitkat <75321987+Fpitkat@users.noreply.github.com>
2022-05-22 07:40:59 -0400

    Services Section

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,48 @@ import Testimonials from "./Components/Testimonials";
 import Team from "./Components/Team";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
+
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
-      <Services />
+      <div className="service--section">
+        <img className="section--heading" src="Images/PetServices.png" alt="" />
+
+        <div className="service--container">
+          <Services
+            icon="./Images/Medical Doctor.png"
+            title="Wellness exams"
+            description="At Sandy Paws Animal Hospital we feel that a healthy pet is a happy pet! Our community has entrusted us with the care of their pets for almost 20 years. We are passionate about our work - and your pets - lifelong healthcare. "
+          />
+          <Services
+            icon="./Images/Catheter.png"
+            title="Vaccinations"
+            description="Sandy Paws Animal Hospital offers routine vaccines, which includes Canine Rabies, Distemper, Parvo, Leptospirosis and Bordetella. Also, feline Rabies, FVRCP, and Leukemia."
+          />
+          <Services
+            icon="./Images/Toothache.png"
+            title="Dentistry"
+            description="Caring for your pet’s teeth and gums is about much more than simply controlling bad breath or enhancing their appearance. We offer routine cleanings with dental radiographs, and extractions if needed."
+          />
+          <Services
+            icon="./Images/Stethoscope.png"
+            title="Diagnostics"
+            description="Sandy Paws Animal Hospital offers a wide range of in-house diagnostic services such as bloodwork, radiographs, ultrasound, ear & skin cytologies, biopsy’s, urinalysis, blood pressure and much more!"
+          />
+          <Services
+            icon="./Images/Clinic.png"
+            title="Surgery"
+            description="Sandy Paws Animal Hospital offers a wide range of surgical services for your pet such as spay, neuter, mass removal, cystotomy, orthopedic, and much more."
+          />
+          <Services
+            icon="./Images/Laser Beam.png"
+            title="Cold laser therapy"
+            description="Cold laser therapy is a noninvasive procedure that uses light to stimulate cell regeneration and increase blood circulation. It helps with wounds, lameness, post surgical, IVDD, and much more. "
+          />
+        </div>
+      </div>
       <Testimonials />
       <div className="team--box">
         <img className="section--heading" src="Images/MeetOurVets.png" alt="" />
--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -1,3 +1,9 @@
-export default function Service() {
-  return;
+export default function Service(props) {
+  return (
+    <div className="service--card">
+      <img className="service--icon" src={props.icon} alt="" />
+      <p className="service--title">{props.title}</p>
+      <p className="service--description">{props.description}</p>
+    </div>
+  );
 }
